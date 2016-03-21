37c81fb137760fe661d1242a6c0e28a60b1400b3
dj2g <dj2g117141@gmail.com>
2016-03-21 14:32:23 -0500

    asd

--- scrapy.py
+++ scrapy.py
@@ -5,26 +5,19 @@ import json
 import random
 import psycopg2
 import itertools
-cont=0
+cont=1621
 conn = psycopg2.connect(dbname='proxys2' ,user='denys', host='localhost' ,password='123456')
 ip='50.201.223.217:80'
 proxy = {"http": "http://"+ip+""}
-def numdni():
-      num1=str(random.randrange(0,10,))
-      num2=str(random.randrange(0,10,))
-      num3=str(random.randrange(0,10,))
-      num4=str(random.randrange(0,10,))
-      num5=str(random.randrange(0,10,))
-      num6=str(random.randrange(0,10,))
-      num7=str(random.randrange(0,10,))
-      num8=str(random.randrange(0,10,))
-      num=str(num1+""+num2+""+num3+""+num4+""+num5+""+num6+""+num7+""+num8)
-      #print (num)
-      return num
+code = '0123456789'
+codigos = itertools.permutations(code,8)
+inicio=0
+fin=30000000
+x = list(itertools.islice(codigos,inicio,fin,1))
 while(cont<=30000000):
       user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36'
       headers={'User-Agent':user_agent,'Host':'consultamiembrodemesa.onpe.gob.pe','Upgrade-Insecure-Requests':'1','Accept':'application/json, text/javascript, */*; q=0.01','Accept-Encoding':'gzip, deflate, sdc','Accept-Language':'es-ES,es;q=0.8','Cache-Control':'no-cache','Connection':'keep-alive','Content-type':'text/html; charset=utf-8','X-Requested-With':'XMLHttpRequest','Cookie':'__cfduid=d12d7eb5e962469337b827edf2afdf7091458311785; _gat=1; _ga=GA1.3.2046105002.1458311788; PHPSESSID=aalhvb2p63ke6bntdu0nhb2le0; ARRAffinity=ceef22ab0852dd7d38577ef3ab3694ae36f68c272effd14941448f1914a17e68'}
-      dni=numdni()
+      dni=str(x[cont][0]+x[cont][1]+x[cont][2]+x[cont][3]+x[cont][4]+x[cont][5]+x[cont][6]+x[cont][7])
       print(dni)
       #dni='31867974'
       r = requests.get('http://consultamiembrodemesa.onpe.gob.pe/consultalv_eg_2016/default/index/consulta?documento='+dni+'&key=as12swerdserdfer4$fsdwesdfwerdswedsde',headers=headers,proxies=proxy,timeout=5)
@@ -52,8 +45,8 @@ while(cont<=30000000):
             #print ("Records created successfully") 
             print(cont)
             #print(sql)
-            cont=cont+1
             #print(cont)
+      cont=cont+1
       print("No participa")
 conn.close()
 print("fin")
