379ce76a58d4cd6f90b0405762de2e936ef0c28b
dj2g <dj2g117141@gmail.com>
2016-03-21 15:53:11 -0500

    asd

--- scrapy.py
+++ scrapy.py
@@ -5,7 +5,7 @@ import json
 import random
 import psycopg2
 import itertools
-cont=4288
+cont=5378
 conn = psycopg2.connect(dbname='proxys2' ,user='denys', host='localhost' ,password='123456')
 ip='50.201.223.217:80'
 proxy = {"http": "http://"+ip+""}
